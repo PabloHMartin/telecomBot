3dfed34ac06171fa7bc1f48520ecab41830424c3
PabloHMartin <hermar65@gmail.com>
2020-08-15 10:58:18 +0200

    feature-functions: new API acc

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true});
 const admin = require('firebase-admin');
-const serviceAccount = require('./phmartinchatbot-firebase-adminsdk-voudy-3a90819e7f.json');
+const serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -26,4 +26,36 @@ exports.dialogflowGateway = functions.https.onRequest((request, response) => {
 
     response.send(result);
   });
+});
+
+
+
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+exports.dialogflowWebhook = functions.https.onRequest(async (request, response) => {
+    const agent = new WebhookClient({ request, response });
+
+    const result = request.body.queryResult;
+
+    async function welcome(agent) {
+      agent.add(`Hola muyayo!`);
+    }
+
+    async function userOnboardingHandler(agent) {
+
+     // Do backend stuff here
+     const db = admin.firestore();
+     const profile = db.collection('users').doc('jeffd23');
+
+     const { name, color } = result.parameters;
+
+      await profile.set({ name, color })
+      agent.add(`Welcome aboard my friend!`);
+    }
+
+
+    let intentMap = new Map();
+    //intentMap.set('UserOnboarding', userOnboardingHandler);
+    intentMap.set('Default Welcome Intent', welcome);
+    agent.handleRequest(intentMap);
 });
